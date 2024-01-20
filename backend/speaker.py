b2dad1f1b371179816d8260ec035ac1a000096d3
ZhiyangLu <>
2024-01-20 18:19:34 +0800

    changes to project

--- backend/speaker.py
+++ backend/speaker.py
@@ -1,7 +1,9 @@
 import os
-from dotenv import load_dotenv
+from playsound import playsound
 import openai
 from pathlib import Path
+from pydub import AudioSegment
+from pydub.playback import play
 
 openai.api_key = "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"
 
@@ -11,6 +13,7 @@ class Speaker:
 
     def generateAudio(self, text):
         speech_file_path = Path(__file__).parent / "../output_audio/output.mp3"
+
         response = openai.audio.speech.create(
             model="tts-1",
             voice="alloy",
@@ -19,6 +22,11 @@ class Speaker:
 
         response.stream_to_file(speech_file_path)
 
+        file = "../output_audio/output.mp3"
+        print('playing sound using native player')
+        os.system("afplay " + file)
+
 if __name__ == "__main__":
     speaker = Speaker()
-    speaker.generateAudio("damn, ivan is a legendary coder")
+    speaker.generateAudio("i hate technical interviews")
+
