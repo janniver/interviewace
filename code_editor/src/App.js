d19493f115a8f34686c6e51646a27598a57c3083
kailashgautham <kailash.gautham@gmail.com>
2024-01-20 17:50:29 +0800

    changes to project

--- code_editor/src/App.js
+++ code_editor/src/App.js
@@ -1,56 +1,8 @@
-import React, { useState,  useEffect } from 'react';
-import './App.css';
-import Button from './components/Button';
-import Editor from 'monaco-editor/react';
+import "./App.css";
+import Landing from "./components/Landing";
 
 function App() {
-  const [srcDoc, setSrcDoc] = useState(` `);
-  const [openedEditor, setOpenedEditor] = useState('html');
-  const onTabClick = (editorName) => {
-    setOpenedEditor(editorName);
-  };
-  const [java, setJava] = useState('');
-  const [cpp, setCpp] = useState('');
-  const [python, setPython] = useState('');
-  useEffect(() => {
-    const timeOut = setTimeout(() => {
-      setSrcDoc(
-        `
-          <html>
-            <body>${java}</body>
-            <style>${cpp}</style>
-            <script>${python}</script>
-          </html>
-        `
-      )
-    }, 250);
-    return () => clearTimeout(timeOut)
-  }, [java, cpp, python])
-  return (
-    <div className="App">
-      <p>Welcome to Interview Practice...</p>
-      <div className="tab-button-container">
-        <Button title="Java" onClick={() => onTabClick('java')}></Button>
-        <Button title="C++" onClick={() => onTabClick('cpp')}></Button>
-        <Button title="Python" onClick={() => onTabClick('python')}></Button>
-      </div>
-      <div className="editor-container">
-        {
-          openedEditor === 'java' ? <Editor language="java" value = {java} setEditorState={setJava}/> 
-          : openedEditor === 'cpp' ? <Editor language="cpp" value = {cpp} setEditorState={setCpp}/>
-          : <Editor language="python" value = {python} setEditorState={setPython}/>
-        }
-      </div>
-      <div>
-        {/* <iframe
-          srcDoc={srcDoc}
-          title="output"
-          sandbox="allow-scripts"
-          width="100%"
-          height="100%"
-        /> */}
-      </div>
-    </div>
-  );
+  return <Landing />;
 }
+
 export default App;
