eba098b8a2b446d4548e30ab5f66137b7a46c033
alfaloo <zhiylu528@gmail.com>
2024-03-26 11:19:11 +0800

    Implement gpt using js

---
+++ js_backend/gpt.mjs
@@ -0,0 +1,33 @@
+import OpenAI from 'openai';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const openai = new OpenAI({
+  apiKey: process.env['GPT_API_KEY'], // This is the default and can be omitted
+});
+
+class GPT {
+
+  constructor(system_msg) {
+    this.messages = []
+    this.messages.push({role: "system", content: system_msg})
+    console.log("Your new assistant is ready!")
+  }
+
+  async chat(input_message) {
+    console.log(input_message);
+    this.messages.push({role: "user", content: input_message})
+    const chatCompletion = await openai.chat.completions.create({
+      messages: this.messages,
+      model: 'gpt-3.5-turbo',
+    });
+    const reply = chatCompletion.choices[0].message.content
+    this.messages.push({role: "assistant", content: reply});
+    return reply
+  }
+
+}
+
+const gpt = new GPT("you are my personal assisstant");
+console.log(await gpt.chat("write me a 100 word poem"))
+
